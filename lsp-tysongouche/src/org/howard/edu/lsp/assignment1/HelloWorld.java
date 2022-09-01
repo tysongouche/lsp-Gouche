5b679ee4cb07e7cc35ecb7c5fc9655e27681777f
tysongouche <tyson.gouche@bison.howard.edu>
2022-09-01 16:35:50 -0400

    This is my commit for assignment number 1.

---
+++ lsp-tysongouche/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,16 @@
+package org.howard.edu.lsp.assignment1;
+
+/** Tyson Gouche
+ * 
+ * @author tysongouche
+ *
+ * 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
+ */
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Large Scale Programming!! (excuse my tardiness)");
+	}
+
+}
